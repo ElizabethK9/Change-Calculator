0481aa0adeba70414a9b83a593d7491303f3279b
Elizabeth Proctor <elizabethproctor66@gmail.com>
2024-05-02 11:39:56 -0700

    Added Validation to the JavaScript Validation to produce an error if the input upon clicking the Calculate button is a negative number.

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,17 @@ function makeChange(amount) {
         document.getElementById('amountError').textContent = 'Amount is required.';
         return;
     }
+    
     // Check if amount is a number
     if (isNaN(amount)) {
         document.getElementById('amountError').textContent = 'Amount must be a number.';
         return;
     }
+
+    // Check if amount is positive
+    if (amount < 0) {
+        document.getElementById('amountError').textContent = 'Amount must be a positive number.';
+        return;
+    }
+
 }
